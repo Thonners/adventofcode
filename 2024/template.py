a11fa11e56b706e2f4ca3fe3caecd220e22c7031
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2024-12-15 18:24:44 +0000

    Add template and wrapper for 2024

---
+++ 2024/template.py
@@ -0,0 +1,7 @@
+def solve(lines: list[str], part: int = 1, test_type="test"):
+    if part == 1:
+        for line in lines:
+            pass
+    else:
+        for line in lines:
+            pass
